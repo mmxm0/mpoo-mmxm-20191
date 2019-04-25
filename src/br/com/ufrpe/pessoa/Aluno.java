8feee0b5125917b9a4d6c0413bc475884ccd0c8f
mmxm0 <yeterorish@hotmail.com>
2019-04-24 22:40:17 -0300

    removendo código que veio da master

--- src/br/com/ufrpe/pessoa/Aluno.java
+++
@@ -1,8 +0,0 @@
-package br.com.ufrpe.pessoa;
-
-public interface Aluno {
-	void setAnoEntrada(int anoEntrada);
-	int getAnoEntrada();
-	void setPeriodoEntrada(int periodoEntrada);
-	int getPeriodoEntrada();
-}
